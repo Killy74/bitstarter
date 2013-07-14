f9041b11a7c79ab584f9403e7dd716807d70fb38
John Smith <example@stanford.edu>
2013-07-14 14:18:43 +0000

    Modified grader and checks

--- grader.js
+++ grader.js
@@ -1,30 +1,10 @@
 #!/usr/bin/env node
-/*
-Automatically grade files for the presence of specified HTML tags/attributes.
-Uses commander.js and cheerio. Teaches command line application development
-and basic DOM parsing.
-
-References:
-
- + cheerio
-   - https://github.com/MatthewMueller/cheerio
-   - http://encosia.com/cheerio-faster-windows-friendly-alternative-jsdom/
-   - http://maxogden.com/scraping-with-node.html
-
- + commander.js
-   - https://github.com/visionmedia/commander.js
-   - http://tjholowaychuk.com/post/9103188408/commander-js-nodejs-command-line-interfaces-made-easy
-
- + JSON
-   - http://en.wikipedia.org/wiki/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON
-   - https://developer.mozilla.org/en-US/docs/JSON#JSON_in_Firefox_2
-*/
 
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
+var HTMLURL_DEFAULT = "http://thawing-atoll-8074.herokuapp.com/";
 var CHECKSFILE_DEFAULT = "checks.json";
 
 var assertFileExists = function(infile) {
@@ -55,6 +35,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkbOject = function(htmlcontent, checksfile) {
+    $ = htmlcontent;
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -65,10 +56,24 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-} else {
+        .option('-u, --url <html_file>', 'URL to index.html', HTMLURL_DEFAULT)
+    .parse(process.argv);
+    if (program.file)  {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	}
+    else {
+	if (program.url){
+	var sys = require('util'),
+    rest = require('restler');
+    rest.get(program.url).on('complete', function(data) {
+	    var outUrl = JSON.stringify(sys.puts(data[0].message), null, 4);
+	    var checkJson = checkObject(outUrl, program.checks);});
+}
+}
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson); 
+}
+else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
