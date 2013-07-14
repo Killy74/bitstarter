bf1e6ee2e8079037b8c301422b593bb627b43901
John Smith <example@stanford.edu>
2013-07-14 11:39:50 +0000

    test2

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 var fs = require('fs');
-    fs.readFile('index.html',function(err,data));
-    if(err) throw err;
+
   response.send("Test");  
 });
 
